7fcb4d2d8f58a034b2374380843a33d323f42a57
s574shinchan <59577293+s574shinchan@users.noreply.github.com>
2026-01-04 21:51:49 +0900

    Update SignUpErrorModule.cs

--- Module/SignUpErrorModule.cs
+++ Module/SignUpErrorModule.cs
@@ -12,7 +12,7 @@ namespace LupeonBot.Module
 {
     public class SignUpErrorModule : InteractionModuleBase<SocketInteractionContext>
     {
-        [SlashCommand("가입문의", "가입안되요 채널에 문의버튼생성")]
+        [SlashCommand("가입문의", "가입안되요 채널에 문의버튼생성 (관리자 전용)")]
         public async Task SignUpErrorNoticeAsync()
         {
             if (Context.User is not SocketGuildUser user || !user.GuildPermissions.Administrator)
@@ -134,4 +134,5 @@ namespace LupeonBot.Module
 
     }
 }
+
 
